6730f9d4946f2d6e1e0436b7071443b4f7385d51
Fevzi Kavalci <fevzi.kavalci@mt-ag.com>
2018-03-20 13:47:26 +0100

    new method addAddress, used also in fillCreatepage

--- src/main/java/operation/ContentOperation.java
+++ src/main/java/operation/ContentOperation.java
@@ -2,6 +2,7 @@ package operation;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import pageobjects.AddressContent;
 import pageobjects.Content;
 
 import java.util.List;
@@ -10,12 +11,23 @@ public class ContentOperation {
 
     private static final String TESTSTRING = "Testinput";
 
+    private static final String TESTZIPCODE = "00000";
+
+    private static final String TESTGEO = "50,00000";
+
+    private static final String TESTEMAIL = "test@test.de";
+
+    private static final String TESTPHONE = "0123456789";
+
+
+
 
     public static void fillCreatePage(Content content) {
 
         List<WebElement> textfields = content.getTextfields();
         List<WebElement> selects = content.getSelects();
-        WebElement continueButton = content.getContinueButton();
+
+        List<WebElement> subPageButtons = content.getSubPageButtons();
 
 
         // Textfelder mit TESTSTRING füllen
@@ -34,11 +46,25 @@ public class ContentOperation {
             }
         }
 
+
+        if (subPageButtons != null) {
+            // Adresse hinzufügen
+            for (WebElement supPageButton : subPageButtons) {
+                System.out.println("ID: " + supPageButton.getAttribute("id"));
+                if (supPageButton.getAttribute("id").contains("AddressSet")) {
+                    UIOperation.click(supPageButton);
+                    addAddress(new AddressContent(content.getPage()));
+                }
+            }
+
+
+        }
+
         String headlineBeforeSubmit = content.getHeadlineText();
 
         // falls vorhanden auf weiter klicken
-        if (continueButton != null) {
-            UIOperation.click(continueButton);
+        if (content.getContinueButton() != null) {
+            UIOperation.click(content.getContinueButton());
         } else
             throw new RuntimeException("Couldn't submit create form. No continue button found.");
 
@@ -47,11 +73,64 @@ public class ContentOperation {
             throw new RuntimeException("Headline after equals headline before click on continue. "
                     + "Form filled incorrectly or entry to create already exists?");
 
-        WebElement saveButton = content.getSaveButton();
-        if (saveButton != null) {
-            UIOperation.click(saveButton);
+
+        if (content.getSaveButton() != null) {
+            UIOperation.click(content.getSaveButton());
         } else
             throw new RuntimeException("Couldn't save create operation. No save button found.");
+
+    }
+
+    public static void addAddress(AddressContent addressContent) {
+
+        // klick auf hinzufügen
+        if (addressContent.getAddButton() == null)
+            throw new RuntimeException("No add button found in address edit page.");
+        UIOperation.click(addressContent.getAddButton());
+
+        // fülle alle textfelder
+        for (WebElement textfield : addressContent.getTextfields()) {
+            if (textfield.getAttribute("id").contains("ZipCode"))
+                UIOperation.typeInTextfield(textfield, TESTZIPCODE);
+            else if (textfield.getAttribute("id").contains("AddressGeo"))
+                UIOperation.typeInTextfield(textfield, TESTGEO);
+            else if (textfield.getAttribute("id").contains("AddressPhone")
+                    || textfield.getAttribute("id").contains("AddressFax"))
+                UIOperation.typeInTextfield(textfield, TESTPHONE);
+            else if (textfield.getAttribute("id").contains("Email"))
+                UIOperation.typeInTextfield(textfield, TESTEMAIL);
+            else
+                UIOperation.typeInTextfield(textfield, TESTSTRING);
+        }
+
+        // nicht leere option aus select wählen
+        for (WebElement select : addressContent.getSelects()) {
+            List<WebElement> options = new Select(select).getOptions();
+            for (WebElement option : options) {
+                if (!(option.getText().equals("") || option.getText().equals(" "))) {
+                    UIOperation.selectOptionFromSelectElement(select, option.getAttribute("value"));
+                    break;
+                }
+            }
+        }
+
+        String headlineBeforeApply = addressContent.getHeadlineText();
+
+        // klicke auf Übernehmen
+        if (addressContent.getApplyButton() == null)
+            throw new RuntimeException("No apply button found in address edit page.");
+        UIOperation.click(addressContent.getApplyButton());
+
+
+        // Test durch headline, ob gleiche seite noch present
+        if (headlineBeforeApply.equals(addressContent.getHeadlineText()))
+            throw new RuntimeException("Headline after equals headline before click on apply. Form filled incorrectly?");
+
+        // klicke auf weiter
+        if (addressContent.getContinueButton() == null)
+            throw new RuntimeException("continue button not found.");
+        UIOperation.click(addressContent.getContinueButton());
+
 
     }
 
