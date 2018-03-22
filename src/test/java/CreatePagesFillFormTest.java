1cfb9d7acc8d59b7e3678db41dd326176a2186f6
Fevzi Kavalci <fevzi.kavalci@mt-ag.com>
2018-03-22 11:39:06 +0100

    GTU Versicherung anlegen

--- src/test/java/CreatePagesFillFormTest.java
+++ src/test/java/CreatePagesFillFormTest.java
@@ -2,22 +2,27 @@ import operation.ContentOperation;
 import operation.PageOperation;
 import operation.UIOperation;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pageobjects.AddressContent;
+import pageobjects.Content;
 import pageobjects.MandatorPage;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 public class CreatePagesFillFormTest {
 
     private static MandatorPage mandatorPage;
+    private Content currentContent;
 
     @BeforeClass
     public static void getMainNavisWithCreateSubmenu() {
         mandatorPage = (MandatorPage) PageOperation.startLoginSelectRole("Mandator", new ChromeDriver());
-
     }
 
     @AfterClass
@@ -25,46 +30,76 @@ public class CreatePagesFillFormTest {
         mandatorPage.quitDriver();
     }
 
+    @Before
+    public void setUp() throws Exception {
+        currentContent = mandatorPage.getCurrentContent();
+    }
+
+    /**
+     * fails, if already exists
+     */
     @Test
     public void fillBankkontoAnlegenFormAndSave() {
 
         PageOperation.clickMainNaviThenSubmenu(mandatorPage, "Bankkonto", "Anlegen");
 
-        ContentOperation.fillCreatePage(mandatorPage.getCurrentContent());
+        ContentOperation.fillCreatePage(currentContent);
 
-        assertThat("headline mismatch. Might not have been saved.", mandatorPage.getCurrentContent().getHeadlineText()
+        assertThat("headline mismatch. Might not have been saved.", currentContent.getHeadlineText()
                 , is("Ihre Daten wurden gespeichert!"));
     }
 
+    /**
+     * fails, if already exists.
+     */
     @Test
     public void fillOrganisationseinheitAnlegen() {
 
         PageOperation.clickMainNaviThenSubmenu(mandatorPage, "Organisationseinheit", "Anlegen");
 
-        ContentOperation.fillCreatePage(mandatorPage.getCurrentContent());
+        ContentOperation.fillCreatePage(currentContent);
 
-        assertThat("headline mismatch. Might not have been saved.", mandatorPage.getCurrentContent().getHeadlineText()
+        assertThat("headline mismatch. Might not have been saved.", currentContent.getHeadlineText()
                 , is("Ihre Daten wurden gespeichert!"));
     }
 
-
+    /**
+     * fails, if already exists
+     */
     @Test//(expected = RuntimeException.class)
     public void fillGTUVersicherungenAnlegenShouldFail() {
         PageOperation.clickMainNaviThenSubmenu(mandatorPage, "GTU-Versicherungen", "Anlegen");
 
         // auf den als erstes aufgelisteten Geldtransportunternehmen klicken, um auf anlegen formular zu gelangen.
-        UIOperation.click(mandatorPage.getCurrentContent().getLinkButtons().get(0));
+        UIOperation.click(currentContent.getLinkButtons().get(0));
 
-        ContentOperation.fillCreatePage(mandatorPage.getCurrentContent());
+        // textfelder mit Zahlenwerte, Datum und string füllen
+        for (WebElement tf : currentContent.getTextfields()) {
+            if (tf.getAttribute("name").contains("sum_")) {
+                UIOperation.typeInTextfield(tf, "10");
+            } else if (tf.getAttribute("name").contains("runTime") || tf.getAttribute("name").contains("Date")) {
+                UIOperation.typeInTextfield(tf, "01.01.2018");
+            } else {
+                UIOperation.typeInTextfield(tf, "Testinput");
+            }
+        }
 
+        ContentOperation.selectNotEmptyOptionOfSelects(currentContent);
 
+        ContentOperation.addAddress(new AddressContent(mandatorPage));
+
+        String headlineBefore = currentContent.getHeadlineText();
+        ContentOperation.clickContinueAndCheckHeadlines(currentContent, headlineBefore);
+        ContentOperation.clickSaveButton(currentContent);
+
+        assertThat(currentContent.getHeadlineText(), containsString("Ihre Daten wurden gespeichert!"));
     }
 
-
-
-    //    @Test
+    // nur um seiten mit anlegen option zu finden
+//        @Test
     public void findPagesWithCreateSubmenu() {
         PageOperation.getMainNavisWithCreateSubmenu(mandatorPage);
     }
+
 
 }
